7044a9a0747dc9d993a240c983a6f9af8d7d1343
William Ganucheau <williamg.online@gmail.com>
2015-06-19 22:12:51 -0500

    Added logging functionality

--- ugly.js
+++ ugly.js
@@ -1,13 +1,61 @@
+// Requires ====================================================================
+var pkg = require ('./package.json');
+var fs = require ('fs');
+
+// Constants ===================================================================
+var VERSION         = pkg.version;
+var LOG_FILE        = 'ugly.log';
+
+// Logging =====================================================================
+function initLog () {
+	// Clear the log
+	fs.writeFileSync (LOG_FILE, "");
+
+	// Write header data
+	writeToFile ('Created with ugly v' + VERSION);
+	writeToFile ('Start date: ' + (new Date ().toString ()));
+	writeToFile ('===========================================================');
+}
+
+function writeToFile (msg_) {
+	fs.appendFileSync (LOG_FILE, msg_ + '\n');
+}
+
+function info (msg_) {
+	var time = new Date ().getTime ();
+
+	msg_ = '[' + time + '] INFO: ' + msg_;
+
+	console.log (msg_);
+	writeToFile (msg_);
+}
+
+function error (msg_) {
+	var time = new Date ().getTime ();
+
+	msg_ = '[' + time + '] ERROR: ' + msg_;
+
+	console.error (msg_);
+	writeToFile (msg_);
+	process.exit (1);
+}
+
+
+// Main code ==================================================================
+
 // Start listening on stdin
 function main () {
+	initLog ();
+	info ("Initializing...");
+	info ("Listening on stdin");
 	readlines (handleLine);
 }
 
 // Listen on stdin and call callback_ on each line
 function readlines (callback_) {
-	console.assert (typeof (callback_) === "function");
+	console.assert (typeof (callback_) === 'function');
 
-	var unhandledText = "";
+	var unhandledText = '';
 
 	process.stdin.setEncoding ('utf8');
 	process.stdin.on ('readable', function () {
@@ -30,7 +78,7 @@ function readlines (callback_) {
 }
 
 function handleLine (line_) {
-	console.assert (typeof (line_) === "string");
+	console.assert (typeof (line_) === 'string');
 
 	// TODO: Handle the line
 }
