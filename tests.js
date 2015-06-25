1a4b3e0c1316df4d95d73a19bbd77a7d6d679af0
William Ganucheau <williamg.online@gmail.com>
2015-06-24 19:22:26 -0500

    Added boundedInt test

--- tests.js
+++ tests.js
@@ -1,5 +1,48 @@
-exports.paramTypeValidation = {
+var sandbox = require ('nodeunit').utils.sandbox;
+var globals = {
+	module: {exports: exports},
+	require: require,
+	console: console
+};
 
+var commands = sandbox ('commands.js', globals);
+var paramTypes = commands.paramTypes;
+
+// Parameter validation ========================================================
+function testValid (res, test) {
+	test.strictEqual (res, undefined);
+}
+
+function testInvalid (res, test) {
+	test.strictEqual (typeof (res), 'string');
+}
+
+exports.paramTypeValidation = {
+	boundedInt: function (test) {
+		var TEST = paramTypes.BOUNDED_INT (0, 10);
+
+		var argList = ['2'];
+		test.strictEqual (TEST.validate (argList), undefined);
+		test.deepEqual (argList, []);
+
+		var argList = ['0', '1', '2'];
+		test.strictEqual (TEST.validate (argList), undefined);
+		test.deepEqual (argList, ['1', '2']);
+
+		var argList = ['10'];
+		test.strictEqual (TEST.validate (argList), undefined);
+		test.deepEqual (argList, []);
+
+		var argList = ['-1'];
+		test.strictEqual (typeof (TEST.validate (argList)), 'string');
+		test.deepEqual (argList, []);
+
+		var argList = ['12'];
+		test.strictEqual (typeof (TEST.validate (argList)), 'string');
+		test.deepEqual (argList, []);
+
+		test.done ();
+	}
 };
 
 exports.paramEvaluation = {
